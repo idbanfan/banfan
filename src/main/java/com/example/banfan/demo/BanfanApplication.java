dc62e50b1760c93439735193012edb4923402348
12645 <12645477446@qq.com>
2020-01-29 16:16:33 +0800

    first commit

---
+++ src/main/java/com/example/banfan/demo/BanfanApplication.java
@@ -0,0 +1,13 @@
+package com.example.banfan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanfanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanfanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/banfan/demo/BanfanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banfan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanfanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
